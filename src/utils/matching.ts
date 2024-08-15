8afd6a33efaa26ff8b8dbf97413baa2b83f1d7e7
Mohamad <mmortada685@gmail.com>
2024-08-15 14:09:02 -0700

    created matching function

---
+++ src/utils/matching.ts
@@ -0,0 +1,33 @@
+"use server"
+import { ChatOpenAI } from "@langchain/openai";
+import { PrismaClient } from "@prisma/client";
+import { cookies } from "next/headers";
+
+export default async function matchUser() {
+    let prisma = new PrismaClient();
+    let allUsers = await prisma.user.findMany({
+        where: {
+            NOT: {
+                id: Number(cookies().get('currentUser')?.value)
+            }
+        }
+    })
+    let currentUser = await prisma.user.findUnique({
+        where: {
+            id: Number(cookies().get('currentUser')?.value)
+        }
+    })
+    let allUsersInterests = allUsers.map((user) => user.keywords);
+    let allUsersIds = allUsers.map((user) => user.id);
+    
+    const llm = new ChatOpenAI({
+        model: "gpt-4o",
+        temperature: 0.8,
+        streaming: true,
+    });
+
+    let message = await llm.invoke(`Find a user with matching interests. Here is the current user's interests: ${currentUser?.keywords.join(', ')}. Here are the ids of all other users: ${allUsersIds} and their interests: ${allUsersInterests.join(', ')}. Respond with ONLY ONE number which would be the user id of the matched user`);
+    console.log(message["content"]);
+    return message["content"];
+}
+    
